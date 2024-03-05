0c97c2c59ae17e04f589778667e356a877ecd698
David Thrane Christiansen <david@davidchristiansen.dk>
2024-03-05 17:00:20 +0100

    feat: more specific serialization and centralized deserialization

--- Tests.lean
+++ Tests.lean
@@ -15,14 +15,4 @@ def main : IO UInt32 := do
   IO.println "Checking that the test project generates at least some deserializable JSON"
   let examples ← loadExamples "demo"
   assert "Examples are non-empty" fun () => !examples.isEmpty
-  let _exs : List (List (String × Example)) ← examples.toList.mapM fun (n, ex) => do
-    let mut out := []
-    match ex.getObj? with
-    | .error err => throw <| IO.userError <| s!"Expected object for {n}: " ++ err
-    | .ok val =>
-      for ⟨k, v⟩ in val.toArray do
-        match Lean.FromJson.fromJson? v with
-        | .error err => throw <| IO.userError <| s!"Failed to deserialize {n}: " ++ err
-        | .ok val => out := (k, val) :: out
-      pure out
   pure 0
