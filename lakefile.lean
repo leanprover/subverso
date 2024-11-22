88819d235a5058aa4b9049d213a2b3493b18887e
David Thrane Christiansen <david@lean-fro.org>
2024-11-22 15:01:23 +0100

    feat: source file highlighting (#61)
    Adds the ability to highlight a whole module.

--- lakefile.lean
+++ lakefile.lean
@@ -33,6 +33,12 @@ lean_exe «subverso-extract» where
   root := `Extract
   supportInterpreter := true
 
+@[default_target]
+lean_exe «subverso-extract-mod» where
+  root := `ExtractModule
+  supportInterpreter := true
+
+
 -- Compatibility shims for older Lake (where logging was manual) and
 -- newer Lake (where it isn't). Necessary from Lean 4.8.0 and up.
 section
@@ -44,6 +50,29 @@ open Lean Elab Command
   if !env.contains `Lake.logInfo then
     elabCommand <| ← `(def $(mkIdent `logInfo) [Pure $(mkIdent `m)] (message : String) : $(mkIdent `m) Unit := pure ())
 end
+
+module_facet highlighted mod : FilePath := do
+  let ws ← getWorkspace
+  let some extract ← findLeanExe? `«subverso-extract-mod»
+    | error "The subverso-extract-mod executable was not found"
+
+  let exeJob ← extract.exe.fetch
+  let modJob ← mod.olean.fetch
+
+  let buildDir := ws.root.buildDir
+  let hlFile := mod.filePath (buildDir / "highlighted") "json"
+
+  exeJob.bindAsync fun exeFile exeTrace =>
+    modJob.bindSync fun _oleanPath modTrace => do
+      let depTrace := mixTrace exeTrace modTrace
+      let trace ← buildFileUnlessUpToDate hlFile depTrace do
+        logStep s!"Exporting highlighted source file JSON for '{mod.name}'"
+        proc {
+          cmd := exeFile.toString
+          args := #[mod.name.toString, hlFile.toString]
+          env := ← getAugmentedEnv
+        }
+      pure (hlFile, trace)
 
 module_facet examples mod : FilePath := do
   let ws ← getWorkspace
@@ -68,6 +97,16 @@ module_facet examples mod : FilePath := do
         }
       pure (hlFile, trace)
 
+library_facet highlighted lib : FilePath := do
+  let ws ← getWorkspace
+  let mods ← lib.modules.fetch
+  let moduleJobs ← BuildJob.mixArray <| ← mods.mapM (fetch <| ·.facet `highlighted)
+  let buildDir := ws.root.buildDir
+  let hlDir := buildDir / "highlighted"
+  moduleJobs.bindSync fun () trace => do
+    pure (hlDir, trace)
+
+
 library_facet examples lib : FilePath := do
   let ws ← getWorkspace
   let mods ← lib.modules.fetch
@@ -75,6 +114,18 @@ library_facet examples lib : FilePath := do
   let buildDir := ws.root.buildDir
   let hlDir := buildDir / "examples"
   moduleJobs.bindSync fun () trace => do
+    pure (hlDir, trace)
+
+
+package_facet highlighted pkg : FilePath := do
+  let ws ← getWorkspace
+  let libs := pkg.leanLibs
+  let exes := pkg.leanExes.map (·.toLeanLib)
+  let libJobs ← BuildJob.mixArray <| ← (libs ++ exes).mapM (fetch <| ·.facet `highlighted)
+  let buildDir := ws.root.buildDir
+  let hlDir := buildDir / "highlighted"
+  libJobs.bindSync fun () trace => do
+    logInfo s!"Highlighted code written to '{hlDir}'"
     pure (hlDir, trace)
 
 package_facet examples pkg : FilePath := do
