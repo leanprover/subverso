7796c5261c5e65b92e3d9693584c963bd4b13368
David Thrane Christiansen <david@davidchristiansen.dk>
2025-05-15 13:00:25 +0200

    compat shim

--- src/SubVerso/Highlighting/Messages.lean
+++ src/SubVerso/Highlighting/Messages.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: David Thrane Christiansen
 -/
 import Lean.Message
+import SubVerso.Compat
 
 namespace SubVerso.Highlighting.Messages
 
@@ -18,11 +19,11 @@ def originallyError (msg : MessageData) : MessageData := .tagged `SubVerso.origi
 Converts the errors in a message log into warnings, recording that they were originally errors for later reconstruction.
 -/
 def errorsToWarnings (log : MessageLog) : MessageLog :=
-  { msgs := log.msgs.map fun m =>
+  SubVerso.Compat.mapMessages log fun m =>
       match m.severity with
         | MessageSeverity.error => { m with severity := MessageSeverity.warning, data := originallyError m.data }
         | _ => m
-  }
+
 
 def severity (msg : Message) : MessageSeverity :=
   if msg.data.hasTag (· == `SubVerso.originallyError) then .error else msg.severity
