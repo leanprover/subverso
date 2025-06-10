8083b28afd6f95c9e94d0f3ae85cd976d7ca6aa7
David Thrane Christiansen <david@davidchristiansen.dk>
2025-06-10 09:57:57 +0200

    feat: support commands in subverso-helper

--- src/SubVerso/Helper.lean
+++ src/SubVerso/Helper.lean
@@ -11,25 +11,43 @@ import SubVerso.Helper.Netstring
 open Lean ToJson FromJson
 open SubVerso.Highlighting
 
+set_option linter.missingDocs true
+
 namespace SubVerso.Helper
 
-/-
+/-!
 This is a synchronous subset of JSON-RPC (someday this might want to go full async, so this will
 provide an easier migration path)
 -/
+
+/--
+Requests that clients can make to the helper
+-/
 inductive Request where
+  /-- A request to elaborate a term in the present environment. -/
   | term (code : String) (type? : Option String)
+  /-- A request to run a command in the present environment. -/
+  | command (code : String)
+  /-- A request that the process terminate -/
   | exit
 deriving Repr
 
+
+/--
+Replies that the helper may return.
+-/
 inductive Result where
+  /-- Highlighted Lean code -/
   | highlighted (code : Highlighted)
 deriving Repr
 
 section
 local instance : Repr Json := ⟨fun x _ => toString x⟩
+/-- Responses to requests may be either valid results or errors. -/
 inductive Response where
+  /-- The requested task was completed, and a result was returned. -/
   | result (res : Result)
+  /-- The requested task was not completed. -/
   | error (code : Nat) (message : String) (data : Option Json)
 deriving Repr
 end
@@ -40,6 +58,9 @@ instance : ToJson Request where
       let params : Json :=
         .mkObj <| [("code", .str code)] ++ (type?.map (fun t => [("type", .str t)])).getD []
       .mkObj [("method", .str "term"), ("params", params)]
+    | .command code =>
+      let params : Json := .mkObj [("code", .str code)]
+      .mkObj [("method", .str "command"), ("params", params)]
     | .exit => .mkObj [("method", .str "exit"), ("params", .mkObj [])]
 
 instance : FromJson Request where
@@ -52,6 +73,10 @@ instance : FromJson Request where
       let type := params.getObjVal? "type" |>.toOption
       let type ← type.mapM (·.getStr?)
       return .term code type
+    | "command" =>
+      let params ← v.getObjVal? "params"
+      let code ← params.getObjValAs? String "code"
+      return .command code
     | "exit" => return .exit
     | _ => throw s!"Didn't understand method '{m}'"
 
@@ -90,12 +115,15 @@ instance : FromJson Response where
       return (← .error <$> err.getObjValAs? _ "code" <*> err.getObjValAs? _ "message" <*> pure data)
     else throw "Expected 'result' or 'error' key"
 
+/-- Sends a message on a stream, encoded in JSON and transported via netstrings. -/
 def send [ToJson α] (stream : IO.FS.Stream) (message : α) : IO Unit :=
   let json := toJson message |>.compress |>.toUTF8
   writeNetstring stream json
 
+/-- Sends a message of the indicated type on a stream, encoded in JSON and transported via netstrings. -/
 def sendThe (α) [ToJson α] (stream : IO.FS.Stream) (message : α) : IO Unit := send stream message
 
+/-- Reads a JSON-encoded message from a stream of netstrings. -/
 def receive [FromJson α] (stream : IO.FS.Stream) : IO (Option α) := do
   let some str ← readNetstring stream
     | return none
@@ -103,4 +131,5 @@ def receive [FromJson α] (stream : IO.FS.Stream) : IO (Option α) := do
   let json ← IO.ofExcept (Json.parse str)
   IO.ofExcept (fromJson? json)
 
+/-- Reads a JSON-encoded message of the given type from a stream of netstrings. -/
 def receiveThe (α) [FromJson α] (stream : IO.FS.Stream) : IO (Option α) := receive stream
