bdc74c95376f9a5c23dba1b3159b03c5c1c8aad7
David Thrane Christiansen <david@lean-fro.org>
2025-07-30 12:45:30 +0200

    feat: hash-consing exported JSON (#154)
    This should make it much smaller, reducing the JSON parsing overhead

--- src/SubVerso/Highlighting.lean
+++ src/SubVerso/Highlighting.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: David Thrane Christiansen
 -/
 import SubVerso.Highlighting.Highlighted
+import SubVerso.Highlighting.Export
 import SubVerso.Highlighting.Code
 import SubVerso.Highlighting.Anchors
 import SubVerso.Highlighting.String
