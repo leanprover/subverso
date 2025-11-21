d18f28592472c88e00db23796af3c35c43e40d38
David Thrane Christiansen <david@davidchristiansen.dk>
2025-11-21 11:15:31 +0100

    fix: warnings and perhaps test failure

--- src/SubVerso/Compat.lean
+++ src/SubVerso/Compat.lean
@@ -237,10 +237,12 @@ set_option linter.unusedVariables false in
 def isModule (stx : Syntax) : Bool :=
   %first_succeeding [HeaderSyntax.isModule ⟨stx⟩, HeaderSyntax.isModule stx, false]
 
+set_option linter.unusedVariables false in
 open CanBeArrayOrList in
 def importModules [CanBeArrayOrList f] (imports : f Import) (opts : Options) (trustLevel : UInt32 := 0) (isModule : Bool := false) : IO Environment :=
   %first_succeeding [
     Lean.importModules (%first_succeeding [asArray imports, asList imports]) opts (trustLevel := trustLevel) (loadExts := true) (level := if isModule then .server else .private),
+    Lean.importModules (%first_succeeding [asArray imports, asList imports]) opts (trustLevel := trustLevel) (loadExts := true),
     Lean.importModules (%first_succeeding [asArray imports, asList imports]) opts (trustLevel := trustLevel)
   ]
 
@@ -398,6 +400,8 @@ abbrev String.takeWhile : String → (Char → Bool) → String :=
     fun s f => (_root_.String.takeWhile s f).copy,
     _root_.String.takeWhile
   ]
+
+set_option linter.unusedVariables false
 
 abbrev String.takeRight : String → Nat → String :=
   %first_succeeding -warning [
